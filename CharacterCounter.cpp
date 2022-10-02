6d86128028117f017d4e66224151d7779d9919f3
nishankkuppa <nishankkuppa@users.noreply.github.com>
2022-10-02 14:36:00 -0700

    Add files via upload

---
+++ CharacterCounter.cpp
@@ -0,0 +1,25 @@
+/*
+* A simple C++ program that counts the number of characters in the string.
+* Demonstrate the use of strings, loops, and various data types. 
+* 
+* @author Nishank Kuppa
+*/
+
+#include <cstdio>
+
+const char string[] = "This is a null-terminated string.";
+
+int main()
+{
+    int count = 0;
+
+    for (int x : string)
+    {
+        if (x == 0) break;
+        ++count;
+    }
+    
+
+    printf("The number of characters is: %d\n", count);
+    return 0;
+}
